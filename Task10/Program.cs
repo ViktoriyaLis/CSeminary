6a4ca02dc20856543bdaf59d2a97dc7ea03b69fd
Viktoriya <vika2113@mail.ru>
2022-12-21 23:10:46 +0900

    Случ число и макс цифра из двух

---
+++ Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Нап прогр, котора выводит случайное число из отрезка (10,99) и показывает наибольшую цифру числа
+
+int GetNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start,end + 1);
+    return number;
+}
+int GetMaxDigit(int number)
+{
+    int max = number % 10;    // последнее число как макс
+    int digit2 = number / 10;
+    
+    if (max < digit2)
+    {
+        max = digit2;
+    }
+    return max;
+}
+
+int RandomNumber = GetNumberFromRange(10,99);
+int maxDigit = GetMaxDigit(RandomNumber);
+Console.WriteLine ($" Для числа {RandomNumber} максимальной цифрой в числе является {maxDigit}");
