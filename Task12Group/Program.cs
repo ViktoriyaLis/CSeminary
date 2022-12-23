e4c1716c9c63fce709d0bd6cd51d7e2646f9cf16
Viktoriya <vika2113@mail.ru>
2022-12-23 22:09:02 +0900

    Остаток деления двух чисел

---
+++ Task12Group/Program.cs
@@ -0,0 +1,15 @@
+﻿// 12 Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine()); // Int.Parse
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1%num2==0)
+{
+    Console.WriteLine("Число кратно");
+}
+else
+{
+    int Ostatok = num1%num2; 
+    Console.WriteLine($"Число {num1}  не кратно {num2}, остаток равен {Ostatok}");
+}
