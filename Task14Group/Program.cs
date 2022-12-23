1402887c1d690fe443edb1ba5d6c9990cc47d609
Viktoriya <vika2113@mail.ru>
2022-12-23 22:26:58 +0900

    Два условия кратно и 7 и 23

---
+++ Task14Group/Program.cs
@@ -0,0 +1,14 @@
+﻿// 14 Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23,н-р 150 и 161
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine()); // Int.Parse
+ 
+if (num % 7 == 0 && num % 23 == 0)     //&& если оба true, || - или один из
+{
+    Console.WriteLine("Число кратно");
+}
+else
+{
+        Console.WriteLine($"Число {num}  не кратно 7 и 23");
+}
+
