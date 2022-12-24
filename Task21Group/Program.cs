468d9a32985f64431a90840ff8733143e9b74122
Viktoriya <vika2113@mail.ru>
2022-12-24 23:11:51 +0900

    квадр уравн для расст между точками

---
+++ Task21Group/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+int getUserValue (string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+double getDistaseCoordinat(int userAx, int userAy, int userBx, int userBy)
+{
+    double result = Math.Sqrt(Math.Pow((userAx-userBx),2) + Math.Pow((userAy-userBy),2));
+    return result;
+}
+
+int userAx = getUserValue("Введите X");
+int userAy = getUserValue("Введите Y");
+int userBx = getUserValue("Введите X");
+int userBy = getUserValue("Введите Y");
+
+double Distanse = getDistaseCoordinat(userAx,userAy,userBx,userBy);
+Console.WriteLine(Distanse);
