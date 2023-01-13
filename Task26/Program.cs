7a523aa23c98418f7c9b5f7268083fa7d386d466
Viktoriya <vika2113@mail.ru>
2023-01-13 19:53:52 +0900

    количество знаков в числе с вайл

---
+++ Task26/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Напишите программу, принимает число и выдает количество цифр в числе.
+546 - 3
+78 - 2
+89562 - 5
+*/
+int GetUserData (string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int UserData = int.Parse (Console.ReadLine()!);
+    return UserData;
+}
+int getNumberOfDigit(int number)
+{
+    int numberOfDigit = 0;
+    while (number>0)
+    {
+        number = number / 10;
+        numberOfDigit ++;
+    }
+    return numberOfDigit;
+}
+void showData (string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(data);
+    Console.ResetColor();
+}
+int number = GetUserData ("Введите число");
+int numberOfDigit = getNumberOfDigit(number);
+showData ($"В числе {number} = ", numberOfDigit);
