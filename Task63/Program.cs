13fab83e4cbcce483d25bb5beb20383293027bee
Viktoriya <vika2113@mail.ru>
2023-02-02 22:41:45 +0900

    рекурсия от 1 до N

---
+++ Task63/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+//N = 5 -> "1, 2, 3, 4, 5"
+//N = 6 -> "1, 2, 3, 4, 5, 6"         
+int GetUserData (string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int UserData = int.Parse (Console.ReadLine()!);
+    return UserData;
+}
+void printRange(int N)
+{
+    if (N==0)
+    {
+        return;
+    }
+    printRange(N-1);
+    Console.Write(N + ", ");
+}
+int userNumber = GetUserData("Введите число");
+printRange(userNumber);
